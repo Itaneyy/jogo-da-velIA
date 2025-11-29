9c5aa8ee45e70b9dadbc9de7eab24fed57fef436
Itaneyy <itaneyaluno2003@gmail.com>
2025-11-29 15:55:17 -0300

    Comentário

--- tictactoe/ai/AIPlayer.java
+++ tictactoe/ai/AIPlayer.java
@@ -1,3 +1,20 @@
+/********************************************************************************************
+ *  AVISO IMPORTANTE — NÃO ALTERAR ESTE ARQUIVO
+ *
+ *  Este arquivo faz parte da ENGINE OFICIAL do desafio de IA de Jogo da Velha.
+ *  Os alunos NÃO estão autorizados a modificar:
+ *
+ *      ✔ Lógica interna
+ *      ✔ Validações
+ *      ✔ Alternância de jogadores
+ *      ✔ Regras de vitória/empate
+ *      ✔ Controle de sleep/tempo
+ *
+ *  Você só deve implementar classes dentro de:  tictactoe.ai
+ *
+ *  Qualquer alteração neste arquivo invalida o desafio.
+ ***********/
+
 package tictactoe.ai;
 
 import tictactoe.core.Board;
